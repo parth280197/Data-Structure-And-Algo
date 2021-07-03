950ba642b912950f224da23923bc8e3d2fc0989c
Parth Patel <parth280197@gmail.com>
2021-07-03 16:35:28 -0500

    Selection Sort Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        int[] unSortedArray = {9,5,1,4,2,10,89};
+        int[] sortedArray = SelectionSort.Sort(unSortedArray);
+        SelectionSort.printArray(sortedArray);
     }
 }
