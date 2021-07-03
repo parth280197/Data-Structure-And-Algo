6b39da1b5807b09eeda47c6db031997e4393cc6b
Parth Patel <parth280197@gmail.com>
2021-07-03 16:35:36 -0500

    Selection Sort Added

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,32 @@
 public class SelectionSort {
+    public static int[] Sort(int[] array) {
+
+        //find min element in array
+        /* swap min element to start of array and maintain two parts in array left sorted and right
+        unsorted */
+
+        for(int i = 0; i <= array.length - 1; i++) {
+            int minIndex = i;
+            for(int j = i + 1 ; j <= array.length - 1; j++) {
+                if(array[i] > array[j]) {
+                    minIndex = j;
+                }
+            }
+
+            int tmp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = tmp;
+        }
+
+        return array;
+    }
+
+    public static void printArray(int[] arr)
+    {
+        int n = arr.length;
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
 }
