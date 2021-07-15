15644cc71c764cda6eaae83507b1c97274109528
Parth Patel <parth280197@gmail.com>
2021-07-14 22:50:40 -0500

    Add array problem Find common elements in three sorted arrays

--- src/ArrayProblems/Utils.java
+++ src/ArrayProblems/Utils.java
@@ -1,5 +1,7 @@
 package ArrayProblems;
 
+import java.util.ArrayList;
+
 public class Utils {
     // https://www.geeksforgeeks.org/check-if-a-key-is-present-in-every-segment-of-size-k-in-an-array/
     public static boolean findxinkindowSize(int N, int[] arr, int x, int k) {
@@ -28,5 +30,31 @@ public class Utils {
             j = 0;
         }
         return true;
+    }
+
+    // https://practice.geeksforgeeks.org/problems/common-elements1132/1
+    public static ArrayList<Integer> CommonElements(int A[], int B[], int C[], int n1, int n2, int n3)
+    {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+
+        int i = 0, j=0, k=0;
+        while(i < n1 && j < n2 && k < n3) {
+            if(A[i] == B[j] && B[j] == C[k] && C[k] == A[i]) {
+                res.add(A[i]);
+                i++;j++;k++;
+            } else if(A[i] < B[j]) {
+                i++;
+            } else if(B[j] < C[k]) {
+                j++;
+            } else {
+                k++;
+            }
+
+            if(i>0 && i<n1) while (A[i] == A[i-1]) { i++; if(i>=n1) break; }
+            if(j>0 && j<n2) while (B[j] == B[j-1]) { j++; if(j>=n2) break; }
+            if(k>0 && k<n3) while (C[k] == C[k-1]) { k++; if(k>=n3) break; }
+        }
+
+        return res;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import ArrayProblems.Utils;
+import java.util.Arrays;
+
 import static ArrayProblems.Utils.findxinkindowSize;
 
 public class Main {
     public static void main(String[] args) {
-        int arr[] = new int[] { 3, 5, 2, 4,  9,  3, 1, 7, 3, 11, 12, 3 };
-        int x = 3, k = 3;
-        int n = arr.length;
-        if (findxinkindowSize(n, arr, x, k))
-            System.out.println("Yes");
-        else
-            System.out.println("No");
+//        int ar1[] = {1, 5, 10, 20, 40, 80};
+//        int ar2[] = {6, 7, 20, 80, 100};
+//        int ar3[] = {3, 4, 15, 20, 30, 70, 80, 120};
+
+        int ar1[] = {3, 3, 3};
+        int ar2[] = {3, 3, 3};
+        int ar3[] = {3, 3, 3};
+        var res = Utils.CommonElements(ar1,ar2,ar3,ar1.length,ar2.length,ar3.length);
+
+        System.out.println(Arrays.toString(res.toArray()));
     }
 }
